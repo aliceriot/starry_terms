896aab1501c10184382c4b5588d93b59f31e5226
Alice Pote <alice.writes.wrongs@gmail.com>
2015-07-24 16:40:20 -0400

    Colors! hooray!

--- starfield.py
+++ starfield.py
@@ -1,5 +1,8 @@
 import curtsies
-from curtsies import FullscreenWindow, FSArray
+import random
+from curtsies import FullscreenWindow, FSArray, Input
+from curtsies.fmtfuncs import *
+from time import sleep
 
 class Starfield():
     def __init__(self):
@@ -16,19 +19,26 @@ class Starfield():
                     self.populate()
 
     def populate(self):
-        numstars = (self.screen.height * self.screen.width) / 10
+        numstars = int((self.screen.height * self.screen.width) / 100)
         for i in range(numstars):
             self.makestar()
+            sleep(1)
+
+    def color(self):
+        return random.choice([cyan, blue, dark, green, magenta, gray, red,yellow])
 
     def makestar(self):
         while True:
             row = random.choice(range(self.screen.height))
             column = random.choice(range(self.screen.width))
-            if (row, column) in used:
+            if (row, column) in self.used:
                 pass
             else:
-                self.array[row, column] = '*'
+                color = self.color()
+                self.array[row, column] = [color('*')]
                 self.screen.render_to_terminal(self.array)
+                self.used += [(row, column)]
+                break
 
 # actually do the thing!
 field = Starfield()
