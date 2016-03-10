2979b460d4e0994b32e4e5a3d2f9a5397e929686
Alice Pote <alice.writes.wrongs@gmail.com>
2016-03-10 13:24:03 -0500

    refactor this a bit

--- starfield.py
+++ starfield.py
@@ -8,7 +8,6 @@ class Starfield():
     def __init__(self):
         self.screen = FullscreenWindow()
         self.array = FSArray(self.screen.height, self.screen.width)
-        self.used = []
 
     def listen(self):
         with Input() as input_generator:
@@ -21,24 +20,37 @@ class Starfield():
     def populate(self):
         numstars = int((self.screen.height * self.screen.width) / 10)
         for i in range(numstars):
-            self.makestar()
+            num = random.choice(range(2))
+            if num == 1:
+                self.make_star()
+            else:
+                self.make_fancy_star()
             sleep(1)
 
     def color(self):
         return random.choice([cyan, blue, dark, green, magenta, gray, red,yellow])
 
-    def makestar(self):
-        while True:
-            row = random.choice(range(self.screen.height))
-            column = random.choice(range(self.screen.width))
-            if (row, column) in self.used:
-                pass
-            else:
-                color = self.color()
-                self.array[row, column] = [color('*')]
-                self.screen.render_to_terminal(self.array)
-                self.used += [(row, column)]
-                break
+    def random_row_and_column(self):
+        dimensions = [self.screen.height, self.screen.width]
+        return list(map(lambda x: random.choice(range(x)), dimensions))
+
+    def place_with_random_color(self, row, column, character):
+        color = self.color()
+        if row < self.array.height and column < self.array.width:
+            self.array[row, column] = [color('*')]
+        self.screen.render_to_terminal(self.array)
+
+    def make_fancy_star(self):
+        row, column = self.random_row_and_column()
+        self.place_with_random_color(row, column+1, '*')
+        self.place_with_random_color(row+1, column, '*')
+        self.place_with_random_color(row+1, column+1, ':')
+        self.place_with_random_color(row+1, column+2, '*')
+        self.place_with_random_color(row+2, column+1, '*')
+
+    def make_star(self):
+        row, column = self.random_row_and_column()
+        self.place_with_random_color(row, column, '*')
 
 # actually do the thing!
 field = Starfield()
